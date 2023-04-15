3a737daf0797f5efa6933c6381ab27c207b6277d
tjdhg456 <hogili89@gist.ac.kr>
2023-04-15 21:17:21 +0900

    --

--- run_test.sh
+++ run_test.sh
@@ -3,4 +3,4 @@ RESOLUTION=0
 python test_agedb.py --seed 0 --gpus 0 --data_dir Face/ --down_size $RESOLUTION --mode ir --checkpoint_dir checkpoint/teacher/resol$RESOLUTION-IR
 
 # Test Code (TinyFace)
-python test_tinyface.py --gpus 0 --tinyface_dir tinyface/ --mode ir --save_dir result/ --checkpoint_path checkpoint/student/F_SKD//resol1-IR/last_net.ckpt
+python test_tinyface.py --gpus 0 --tinyface_dir tinyface/ --mode ir --save_dir result/ --checkpoint_path checkpoint/student/F_SKD/resol1-IR/last_net.ckpt
